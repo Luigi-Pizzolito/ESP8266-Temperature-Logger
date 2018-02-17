4165d99ae08a272ff951ca0dc369494bdc0db1e3
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2018-02-17 23:50:44 +0800

    Basic Code in Place

---
+++ ESP8266-Temperature-Logger.ino
@@ -0,0 +1,179 @@
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+
+
+const char* ssid = "27-2";
+const char* password = "1357924680";
+const char* host = "maker.ifttt.com";
+
+OneWire  ds(2);  // on pin 2 (a 4.7K resistor is necessary)
+ADC_MODE(ADC_VCC);
+
+void setup() {
+  pinMode(16, WAKEUP_PULLUP);
+  WiFi.persistent(false);
+  Serial.begin(115200);
+  delay(10);
+
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+
+  if ( !ds.search(addr)) {
+    Serial.println("No more addresses.");
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+
+  Serial.print("ROM =");
+  for ( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+    Serial.println("CRC is not valid!");
+    return;
+  }
+  Serial.println();
+
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  }
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+
+  present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE);         // Read Scratchpad
+
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  Serial.print(" Celsius, ");
+  Serial.print(fahrenheit);
+  Serial.println(" Fahrenheit");
+
+  uint32_t getVcc = ESP.getVcc();
+  String VccVol = String((getVcc / 1000U) % 10) + "." + String((getVcc / 100U) % 10) + String((getVcc / 10U) % 10) + String((getVcc / 1U) % 10);
+  Serial.print("Battery Voltage: ");
+  Serial.print(VccVol);
+  Serial.println("C");
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // We now create a URI for the request
+  String url = "/trigger/TinyTempLogger/with/key/hEPx88jPBFo8rhXT9Aav-8xzp18Nrd9y3Y4oMOr-FMk?value1=" + String(celsius) + "&value2=" + VccVol;
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  delay(10);
+
+  // Read all the lines of the reply from server and print them to Serial
+  Serial.println("Respond:");
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+
+  //delay(120000);
+  WiFi.disconnect();
+  WiFi.mode(WIFI_OFF);
+  WiFi.forceSleepBegin();
+
+  Serial.println("Deep sleep...");
+  ESP.deepSleep(10 * 60 * 1000000, WAKE_RF_DEFAULT);
+  delay(1000);
+}
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A simple wireless temperature logger. Publishes data via GET requests.
         - [Uploading Data](#uploading-data)
         - [Power Saving](#power-saving)
         - [Configuration](#configuration)
-    - [Usefull Links and References](#usefull-links-and-references)
+    - [Useful Links and References](#useful-links-and-references)
 
 ## Planned Features (And Progress)
  - [ ] Uploads
@@ -15,6 +15,7 @@ A simple wireless temperature logger. Publishes data via GET requests.
  - [ ] Configuration
 
 ## About
+Designed to be simple and easy to use. Will simply log temperature at the specified interval and send the values over to a IOT Server.
 
 ### Uploading Data
 
@@ -23,4 +24,35 @@ A simple wireless temperature logger. Publishes data via GET requests.
 ### Configuration
 
 
-## Usefull Links and References
+## Useful Links and References
+- ESP Info
+    - Pinouts
+        - [ESP-12 Pinout](https://esp8266.github.io/Arduino/versions/2.0.0/doc/esp12.png)
+        - [ESP-01 Pinout](https://os.mbed.com/media/uploads/sschocke/esp8266-pinout_etch_copper_top.png)
+    - Programming
+        - [ESP-01](https://os.mbed.com/users/sschocke/code/WiFiLamp/wiki/Updating-ESP8266-Firmware)
+    - General Guides
+        - [Sparkfun Guide](https://learn.sparkfun.com/tutorials/esp8266-thing-hookup-guide/using-the-arduino-addon)
+        - [Beginner's Guide](https://github.com/tttapa/ESP8266)
+            - [Introduction](https://tttapa.github.io/ESP8266/Chap01%20-%20ESP8266.html)
+            - [Hardware](https://tttapa.github.io/ESP8266/Chap02%20-%20Hardware.html)
+            - [Software](https://tttapa.github.io/ESP8266/Chap03%20-%20Software.html)
+            - [Microcontroller](https://tttapa.github.io/ESP8266/Chap04%20-%20Microcontroller.html)
+            - [Network Protocols](https://tttapa.github.io/ESP8266/Chap05%20-%20Network%20Protocols.html)
+            - [Uploading](https://tttapa.github.io/ESP8266/Chap06%20-%20Uploading.html)
+            - [Wi-Fi connections](https://tttapa.github.io/ESP8266/Chap07%20-%20Wi-Fi%20Connections.html)
+            - [mDNS](https://tttapa.github.io/ESP8266/Chap08%20-%20mDNS.html)
+            - [Web Servers](https://tttapa.github.io/ESP8266/Chap09%20-%20Web%20Server.html)
+            - [Simple Web Server](https://tttapa.github.io/ESP8266/Chap10%20-%20Simple%20Web%20Server.html)
+            - [SPIFFS](https://tttapa.github.io/ESP8266/Chap11%20-%20SPIFFS.html)
+            - [Uploading to the Server](https://tttapa.github.io/ESP8266/Chap12%20-%20Uploading%20to%20Server.html)
+            - [OTA](https://tttapa.github.io/ESP8266/Chap13%20-%20OTA.html)
+            - [WebSocket](https://tttapa.github.io/ESP8266/Chap14%20-%20WebSocket.html)
+            - [NTP](https://tttapa.github.io/ESP8266/Chap15%20-%20NTP.html)
+            - [Data Logging](https://tttapa.github.io/ESP8266/Chap16%20-%20Data%20Logging.html)
+            - [Email Notifier](https://tttapa.github.io/ESP8266/Chap17%20-%20Email%20Notifier.html)
+            - [Advanced](https://tttapa.github.io/ESP8266/Chap18%20-%20Advanced.html)
+            - [Conclusion](https://tttapa.github.io/ESP8266/Chap19%20-%20In%20Conclusion.html)
+- GitHub Markdown Info
+    - [Formatting and Syntax](https://help.github.com/articles/basic-writing-and-formatting-syntax/)
+    - [Emoji Cheat Sheet](https://www.webpagefx.com/tools/emoji-cheat-sheet/)
